73eb6c74e483aa33da7f0448e24f506c1ff9d8f9
Your Name <freelancermd82@gmail.com>
2024-07-10 13:51:29 +0600

    register alreat

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -4,14 +4,17 @@ import app from '../../firebase/firebase.config';
 
 
 
+
 const auth = getAuth(app);
 
 
 const Register = () => {
-    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        setSuccess('');
         const email = event.target.email.value;
         const password = event.target.password.value;
         //  console.log(event.target.email.value)
@@ -20,21 +23,27 @@ const Register = () => {
         .then(result => {
             const loggedUser = result.user;
             console.log(loggedUser);
-            setEmail(loggedUser);
+            setError('');
+            event.target.reset();
+            setSuccess('User has created successfully');
+
         })
         .catch(error => {
-            console.error(error);
+            console.error(error.message);
+            setError(error.message)
+           
         })
     }
 
 
     const handleEmailChange = (event) =>{
         console.log(event.target.value);
-        setEmail(event.target.value);
+        // setEmail(event.target.value);
     }
 
     const handlePasswordBlur = (event) =>{
         console.log(event.target.value);
+        
     }
 
 
@@ -42,12 +51,14 @@ const Register = () => {
         <div className='w-50 mx-auto mt-5'>
             <h3>Please Register</h3>
             <form onSubmit={handleSubmit}>
-                <input className='w-50 mb-4 ps-2 rounded' onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' />
+                <input className='w-50 mb-4 ps-2 rounded' onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' required/>
                 <br />
-                <input className='w-50 mb-4 ps-2 rounded' onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' />
+                <input className='w-50 mb-4 ps-2 rounded' onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' required />
                 <br />
                 <input className='btn btn-primary' type="submit" value="Register" />
             </form>
+            <p className='text-danger'>{error}</p>
+            <p className='text-success'>{success}</p>
         </div>
     );
 };

4a4b79c60b2ad75ddfed36d3b70dc04718b7d0f6
Your Name <freelancermd82@gmail.com>
2024-07-10 08:46:36 +0600

    login register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        //  console.log(event.target.email.value)
+        console.log(email, password);
+    }
+
+
+    const handleEmailChange = (event) =>{
+        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = (event) =>{
+        console.log(event.target.value);
+    }
+
+
     return (
         <div>
-            <h3>This is register page.</h3>
+            <h3>Please Register</h3>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' />
+                <br />
+                <input onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' />
+                <br />
+                <input type="submit" value="Register" />
+            </form>
         </div>
     );
 };
